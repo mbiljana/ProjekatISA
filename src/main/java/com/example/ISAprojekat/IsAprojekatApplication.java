1c0e6742ef064014ccad6dca9141ea660af26906
mbiljana <mbiljana99@gmail.com>
2021-11-19 16:09:30 +0100

    db

--- src/main/java/com/example/ISAprojekat/IsAprojekatApplication.java
+++ src/main/java/com/example/ISAprojekat/IsAprojekatApplication.java
@@ -2,12 +2,18 @@ package com.example.ISAprojekat;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication (exclude = {DataSourceAutoConfiguration.class})
 public class IsAprojekatApplication {
 
-	public static void main(String[] args) {
+	public static void main (String[] args) {
 		SpringApplication.run(IsAprojekatApplication.class, args);
+
+
+
+
+
 	}
 
 }
--- src/main/java/com/example/ISAprojekat/Model/Boat.java
+++ src/main/java/com/example/ISAprojekat/Model/Boat.java
@@ -1,9 +1,18 @@
 package com.example.ISAprojekat.Model;
 
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import javax.persistence.*;
 
 @Entity
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class Boat {
 
     @Id
