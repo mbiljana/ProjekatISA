f09eb2d0a98c14898f0b0434b46622754eb60679
AnamarijaD <anamarijadolovac@gmail.com>
2021-11-26 21:35:42 +0100

    added homepage

--- src/main/resources/data-mysql.sql
+++ src/main/resources/data-mysql.sql
@@ -1,2 +1,5 @@
+INSERT INTO korisnik(id,name,surname,emailAddress,phoneNumber,city,state,homeAddress,birthDate,username,password)
+VALUES (2,'Biljana','Marinkov','bilja@gmail.com','061123548','Novi Sad','Serbia','Kralja Petra 23','1999-05-02','bilja123','ftn');
+
 INSERT INTO cottage(id,cottage_additional_services,cottage_address,cottage_description,cottage_name,cottage_rules,num_beds,num_rooms)
-VALUES (118,'#petfriendly','Zlatibor, Zlatiborska 112a','Small cottage in the woods.','Cottage1','No smoking',3,3);
+VALUES (1,'#petfriendly','Zlatibor, Zlatiborska 112a','Small cottage in the woods.','Cottage1','No smoking',3,3);
